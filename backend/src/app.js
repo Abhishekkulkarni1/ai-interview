89f7e00abc4b946a2dc0604caf888074752d2370
Abhishekkulkarni1 <11432abhi@gmail.com>
2026-05-23 15:21:27 +0530

    updated frontend url in backend cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-interview-frontend-yo3l.onrender.com"
+    ],
     credentials: true,
   }),
 );
